482a2a645cae2f4a04594c735a9fb0953bfd702a
ThaCaper <43811142+ThaCaper@users.noreply.github.com>
2019-03-27 13:27:04 +0100

    Fixed the most problems

--- AttApp/src/attapp/dal/Authentication.java
+++ AttApp/src/attapp/dal/Authentication.java
@@ -15,33 +15,11 @@ import java.sql.SQLException;
  */
 public class Authentication
 {
-<<<<<<< HEAD
 
-//public static boolean validateStudentLogin(String sLogin, String password)
-//{
-//    if(sLogin.equals("Wilhelm Hansen") && password.equals("xsdc"))
-//            {
-//                return true;
-//            }
-//    
-//    
-//    return false;
-//}
-//
-//public static boolean validateTeacherLogin(String tLogin, String password)
-//{
-//    if(tLogin.equals("Hans Jensen") && password.equals("xxcc"))
-//    {
-//        return true;
-//    }
-//    
-//    return false;
-    public Student loginStudent(String email, String password) throws SQLServerException, IOException, SQLException
-=======
-    
+
 public static boolean validateStudentLogin(String sLogin, String password)
 {
-    if(sLogin.equals("Wilhelm Hansen") && password.equals("elev"))
+    if(sLogin.equals("Wilhelm Hansen") && password.equals("xsdc"))
             {
                 return true;
             }
@@ -52,9 +30,16 @@ public static boolean validateStudentLogin(String sLogin, String password)
 
 public static boolean validateTeacherLogin(String tLogin, String password)
 {
-    if(tLogin.equals("Hans Jensen") && password.equals("lære"))
->>>>>>> 4511cf4bcd318b0739929aa2837271cbd5b0c9b6
+    if(tLogin.equals("Hans Jensen") && password.equals("xxcc"))
     {
+        return true;
+    }
+    
+    return false;
+}
+
+        public Student loginStudent(String email, String password) throws SQLServerException, IOException, SQLException
+        {
         DbConnection dc = new DbConnection();
         try (Connection con = dc.getConnection(); PreparedStatement pstmt = con.prepareStatement("Select 'Email_stud', 'password' FROM LoginStud WHERE Email_stud = (?) AND password = (?)");)
         {
